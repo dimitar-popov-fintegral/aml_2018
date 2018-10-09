d767a2cc78d2c92fc3678d2b15b2214007f5af6a
dimitar-popov-fintegral <dimitar.popov@fintegral.com>
2018-10-10 00:34:11 +0200

    changes
    1.- added feature transforms sin, pow(2)
    2.- added per feature outlier detection
    3.- submitted results meet hard baseline

--- research/linear_regression.py
+++ research/linear_regression.py
@@ -71,99 +71,104 @@ def clean_data(predictors, response, clean_mode):
 
 
 #######################################################################
-## READ and CLEAN data
+def main():
+    
+    #######################################################################
+    ## READ and CLEAN data
+    #######################################################################
+
+    predictors = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'X_train.csv'), header=0, index_col=0)
+    zero_variance = predictors.columns[predictors.describe().transpose()['std']<1e-6]
+    predictors = predictors.reindex(columns=predictors.columns.difference(zero_variance))
+    test_predictors = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'X_test.csv'), header=0, index_col=0)
+
+    response = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'y_train.csv'), header=0, index_col=0)
+
+    train_data = clean_data(predictors, response, clean_mode=CLEAN_MODE.RESPONSE)
+    predict_data = clean_data(test_predictors, pd.DataFrame(index=test_predictors.index, columns=['y'], data=0), clean_mode=CLEAN_MODE.ZERO)
+
+
+    #######################################################################
+    ## RUN REGRESSION
+    #######################################################################
+
+    in_sample_cnt = 1200
+    pca_num_components = None
+    importance_threshold = 0.0015
+    predict = True
+
+    y = train_data[response.columns].values
+    y_ = y[0:in_sample_cnt]
+    y_test = y[in_sample_cnt:]
+
+    if importance_threshold is not None:
+        model = ExtraTreesClassifier()
+        model.fit(train_data[predictors.columns].values, train_data[response.columns].values)
+        importance = pd.Series(index=predictors.columns, data=model.feature_importances_)
+        features = importance[importance >= importance_threshold].index
+    else:
+        features = predictors.columns
+
+    print("Using %d features" % len(features))
+    X = train_data[features].values
+    X_predict = predict_data[features].values
+    X_train = X[0:in_sample_cnt]
+    X_test = X[in_sample_cnt:]
+
+
+    if pca_num_components is not None:
+        pca = PCA(n_components=min(pca_num_components, len(features)))
+        pca.fit(X_train)
+        ev = pca.explained_variance_ratio_
+        print("Explained variance: %s, (total: %s)" % (ev, sum(ev)))
+        X_train = pca.transform(X_train)
+        X_predict = pca.transform(X_predict)
+        if len(X_test) > 0:
+            X_test = pca.transform(X_test)
+
+
+    # Create linear regression object
+    lm = linear_model.LinearRegression(normalize=True)
+    # Train the model using the training sets
+    lm.fit(X_train, y_)
+
+    if predict:
+        comment = "PCA_%s_Importance_%s(%s)_DataCnt_%s" % (pca_num_components, importance_threshold, len(features), in_sample_cnt)
+
+        print('predict and write to pandas Series object')
+        y = lm.predict(X_predict)
+        write_to_file = pd.Series(y.flatten(), index=predict_data.index, name='y')
+        write_to_file.to_csv(os.path.join(dt.output_dir(), 'task1_solution_{}.csv'.format(comment)), index=True, header=['y'], index_label=['id'])
+
+
+    if len(y_test) > 0:
+
+        # Make predictions using the testing set
+        y_fill = lm.predict(X_test)
+
+        # The coefficients
+        print('Coefficients: \n', lm.coef_)
+        # The mean squared error
+        print("Mean squared error: %.2f"
+              % mean_squared_error(y_test, y_fill))
+        # Explained variance score: 1 is perfect prediction
+        print('Variance score: %.2f' % r2_score(y_test, y_fill))
+
+        mpl.style.use('bmh')
+        plt.figure()
+        plt.plot(y_fill - y_test, 'o')
+        plt.title('Residuals')
+
+        plt.figure()
+        plt.scatter(y_fill, y_test, color='black')
+        plt.title('Predicted vs. Actual')
+
+        plt.show()
+
 #######################################################################
+if __name__ == '__main__':
 
-predictors = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'X_train.csv'), header=0, index_col=0)
-zero_variance = predictors.columns[predictors.describe().transpose()['std']<1e-6]
-predictors = predictors.reindex(columns=predictors.columns.difference(zero_variance))
-test_predictors = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'X_test.csv'), header=0, index_col=0)
-
-response = pd.read_csv(os.path.join(dt.data_dir(), 'task1', 'y_train.csv'), header=0, index_col=0)
-
-train_data = clean_data(predictors, response, clean_mode=CLEAN_MODE.RESPONSE)
-predict_data = clean_data(test_predictors, pd.DataFrame(index=test_predictors.index, columns=['y'], data=0), clean_mode=CLEAN_MODE.ZERO)
-
-
-#######################################################################
-## RUN REGRESSION
-#######################################################################
-
-in_sample_cnt = 1200
-pca_num_components = None
-importance_threshold = 0.0015
-predict = True
-
-y = train_data[response.columns].values
-y_ = y[0:in_sample_cnt]
-y_test = y[in_sample_cnt:]
-
-if importance_threshold is not None:
-    model = ExtraTreesClassifier()
-    model.fit(train_data[predictors.columns].values, train_data[response.columns].values)
-    importance = pd.Series(index=predictors.columns, data=model.feature_importances_)
-    features = importance[importance >= importance_threshold].index
-else:
-    features = predictors.columns
-
-print("Using %d features" % len(features))
-X = train_data[features].values
-X_predict = predict_data[features].values
-X_train = X[0:in_sample_cnt]
-X_test = X[in_sample_cnt:]
-
-
-if pca_num_components is not None:
-    pca = PCA(n_components=min(pca_num_components, len(features)))
-    pca.fit(X_train)
-    ev = pca.explained_variance_ratio_
-    print("Explained variance: %s, (total: %s)" % (ev, sum(ev)))
-    X_train = pca.transform(X_train)
-    X_predict = pca.transform(X_predict)
-    if len(X_test) > 0:
-        X_test = pca.transform(X_test)
-
-
-# Create linear regression object
-lm = linear_model.LinearRegression(normalize=True)
-# Train the model using the training sets
-lm.fit(X_train, y_)
-
-if predict:
-    comment = "PCA_%s_Importance_%s(%s)_DataCnt_%s" % (pca_num_components, importance_threshold, len(features), in_sample_cnt)
-
-    print('predict and write to pandas Series object')
-    y = lm.predict(X_predict)
-    write_to_file = pd.Series(y.flatten(), index=predict_data.index, name='y')
-    write_to_file.to_csv(os.path.join(dt.output_dir(), 'task1_solution_{}.csv'.format(comment)), index=True, header=['y'], index_label=['id'])
-
-
-if len(y_test) > 0:
-
-    # Make predictions using the testing set
-    y_fill = lm.predict(X_test)
-
-    # The coefficients
-    print('Coefficients: \n', lm.coef_)
-    # The mean squared error
-    print("Mean squared error: %.2f"
-          % mean_squared_error(y_test, y_fill))
-    # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.2f' % r2_score(y_test, y_fill))
-
-    mpl.style.use('bmh')
-    plt.figure()
-    plt.plot(y_fill - y_test, 'o')
-    plt.title('Residuals')
-
-    plt.figure()
-    plt.scatter(y_fill, y_test, color='black')
-    plt.title('Predicted vs. Actual')
-
-    plt.show()
-
-
-
+    main()
 
 
 
---
+++ research/task1_scratch.py
@@ -0,0 +1,120 @@
+#######################################################################
+def gmm(X, model, n_components, prior, covariance_type):
+
+	from sklearn import mixture
+
+	n, m = X.shape
+
+	if model == 'GMM':
+		mm = mixture.GaussianMixture(
+			n_components=n_components,
+			covariance_type=covariance_type,
+			max_iter=100
+		).fit(X)
+	elif model == 'dirichlet_process_GMM':
+		if covariance_type=='diag' or covariance_type == 'spherical':
+			cov_prior = 1e0
+		else:
+			cov_prior * numpy.eye(2)
+
+		mm = mixture.BayesianGaussianMixture(
+			n_components=n_components,
+			covariance_type=covariance_type,
+			weight_concentration_prior=numpy.power(10., prior),
+			weight_concentration_prior_type='dirichlet_process',
+			max_iter=100,
+			random_state=12357).fit(X)
+
+	else:
+		raise IOError('invalid model type input')
+
+	covariance = numpy.zeros((n_components, m, m))
+	for i in range(n_components):
+		if covariance_type == 'full':
+			covariances[i] = mm.covariances_[i]
+		elif covariance_type == 'tied':
+			covariances[i] = mm.covariances_
+		elif covariance_type == 'diag':
+			covariances[i] = numpy.diag(mm.covariances_[i])
+		elif covariance_type == 'spherical':
+			covariances[i] = numpy.eye(mm.means_.shape[1]) * mm.covariances_[i]
+
+	return X, mm.predict(X), mm.means_, covariances, title
+
+
+#######################################################################
+def classification(comment=''):
+
+	logger = logging.getLogger(__name__)
+
+	##
+	logger.info('read provided data')
+	X_test, X_train, y_train = read_data()
+	scale_mean = X_train.mean()
+	scale_std = X_train.std()
+
+	# ##
+	# logger.info('basic pre-processing of class labels')
+	# check_obs_per_age = {i: sum(y_train.y == i) for i in y_train.y.unique()}
+	# remove_low_obs_per_age = {i: j for i, j in check_obs_per_age.items() if j < 7}
+	# mask = ~y_train.y.isin(remove_low_obs_per_age.keys())
+
+	# y_train = y_train[mask]
+	# std_X_train = std_X_train[mask]
+	# del mask
+
+	##
+	logger.info('standardize using mean and std. dev. of observed samples')
+	std_X_train = (X_train - scale_mean) / scale_std
+	std_X_test = (X_test - scale_mean) / scale_std
+	y_train.index = y_train.index.astype(int)
+	y_train.y = y_train.y.astype(int)
+
+
+	## 
+	logger.info('basic pre-processing of class labels')
+	check_obs_per_age = {i: sum(y_train.y == i) for i in y_train.y.unique()}
+	remove_low_obs_per_age = {i: j for i, j in check_obs_per_age.items() if j < 7}
+	mask = ~y_train.y.isin(remove_low_obs_per_age.keys())
+
+	y_train = y_train[mask]
+	std_X_train = std_X_train[mask]
+	del mask
+
+	##
+	logger.info('fill NaN with 0 i.e. the mean of the standardized random variables')
+	std_X_train.fillna(0, inplace=True)
+	std_X_test.fillna(0, inplace=True)
+
+
+	##
+	logger.info('grid search for optimum parameter \'alpha\'') 
+	mlpclass = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(3), alpha=1e-5, random_state=12357, max_iter=1e3)
+	paramters = {
+		'hidden_layer_sizes': numpy.arange(3, 5, 1),
+		'alpha': 10.0 ** -numpy.arange(1, 5, 1.0)
+	}
+	optMPL = GridSearchCV(mlpclass, paramters)
+	optMPL.fit(std_X_train, y_train.values.flatten())
+
+	##
+	logger.info('prediction in-sample')
+	opt_y_predict = optMPL.predict(std_X_train)
+
+	##
+	logger.info('produce classification statistics')
+	dim = len(opt_y_predict)
+	inSampleConfusionMatrix = confusion_matrix(y_train, opt_y_predict)
+	accuracy = numpy.sum(numpy.diag(inSampleConfusionMatrix)) / dim
+	logger.info("Using optimal parameter alpha, model accuracy {:.4f} ".format(accuracy))
+
+	##
+	logger.info('final prediction')
+	opt_y_hat_predict = optMPL.predict(std_X_test)
+
+	##
+	logger.info('write to pandas Series object')
+	write_to_file = pandas.Series(opt_y_hat_predict, index=X_test.index.astype(int), name='y')
+	write_to_file.to_csv(os.path.join(dt.output_dir(), 'task1_solution_{}.csv'.format(comment)), index=True, header=['y'], index_label=['id'])
+
+
