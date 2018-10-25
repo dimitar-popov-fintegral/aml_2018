741f24db0dac72f727b45d65019ad6c3acc7b118
marcin <mr.snarski@gmail.com>
2018-10-25 19:14:22 +0200

    example double run

--- research/boosting.py
+++ research/boosting.py
@@ -15,8 +15,6 @@ sys.path.append(os.path.join(THIS_DIR, '..'))
 import matplotlib.pyplot as plt 
 import data as dt
 
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -25,8 +23,8 @@ from sklearn.model_selection import StratifiedKFold
 
 
 #######################################################################
-def train_ada_boost_classifier(x_train, y_train, x_test, y_test, max_depth, 
-    n_estimators, learning_rate_lower, learning_rate_upper, learning_rate_num, comment='AdaBoostClassifier'):
+def train_ada_boost_classifier(x_train, y_train, x_test, y_test, max_depth,
+                               n_estimators, learning_rate_lower, learning_rate_upper, learning_rate_num, comment='AdaBoostClassifier'):
 
     logger = logging.getLogger(__name__)
     rs = numpy.random.RandomState(12357)
@@ -40,55 +38,24 @@ def train_ada_boost_classifier(x_train, y_train, x_test, y_test, max_depth,
     opt_ada_boost_params = grid_search.fit(x_train, y_train.values.flatten())
     logger.info("Best: [{:f}] using [{}]".format(opt_ada_boost_params.best_score_, opt_ada_boost_params.best_params_))
 
-    check_score = balanced_accuracy_score(opt_ada_boost_params.predict(x_test), y_test.values.flatten())
+    prediction = opt_ada_boost_params.predict(x_test)
+
+    check_score = balanced_accuracy_score(prediction, y_test.values.flatten())
     logger.info('Check prediction score on validation set := [{:f}]'.format(check_score))
 
-    ##
-    logger.info('Refit AdaBoostClassifier w/ best params from CV')
-    ada_boost_classifier = AdaBoostClassifier(
-        DecisionTreeClassifier(max_depth=max_depth), 
-        n_estimators=opt_ada_boost_params.best_params_['n_estimators'], 
-        learning_rate=opt_ada_boost_params.best_params_['learning_rate']
-    )
-    
-    ##
-    ada_boost_classifier.fit(x_train, y_train.values.flatten())
-    score = balanced_accuracy_score(ada_boost_classifier.predict(x_test), y_test.values.flatten())
-    logger.info('Prediction score on validation set := [{:f}]'.format(score))
+    return prediction
 
-    ada_boost_classifier_test_accuracy = []
-    for ada_boost_classifier_test_predict in ada_boost_classifier.staged_predict(x_test):
-        ada_boost_classifier_test_accuracy.append(balanced_accuracy_score(
-            y_true=ada_boost_classifier_test_predict, 
-            y_pred=y_test.y.values.flatten()))
+"""
+x_train, x_validate = choose_validation_set
+y_train, y_validate = choose_validation_set
 
-    y_submit = ada_boost_classifier.predict(x_submit)
-    output = pandas.Series(y_submit, name='y')
-    output.to_csv(os.path.join(dt.output_dir(), 'AdaBoost_{:s}.csv'.format(comment)), index=True, header=['y'], index_label=['id'])
+predict = train_ada_boost_classifier(x_train, y_train, x_validate, y_validate, CONST_PARAMS)
 
-    y_check_submit = opt_y_check_submitada_boost_params.predict(x_submit)
-    output = pandas.Series(y_check_submit, name='y')
-    output.to_csv(os.path.join(dt.output_dir(), 'AdaCheck_{:s}.csv'.format(comment)), index=True, header=['y'], index_label=['id'])
+predict = train_ada_boost_classifier(x_train+x_validate, y_train+y_validate, x_test, y_all_zeros, CONST_PARAMS)
+"""
 
-    # Boosting might terminate early, but the following arrays are always
-    # n_estimators long. We crop them to the actual number of trees here:
-    ada_boost_classifier_num_trees = len(ada_boost_classifier)
-    ada_boost_classifier_estimator_errors = ada_boost_classifier.estimator_errors_[:ada_boost_classifier_num_trees]
 
-    ##
-    logger.info('plotting results')
-    plt.ioff()
-    fig = plt.figure(figsize=(15, 15))
-    plt.plot(
-        range(1, ada_boost_classifier_num_trees + 1),
-         ada_boost_classifier_test_accuracy, c='black',
-         linestyle='dashed', label='AdaBoostClassifier with SAMME.R algo.'
-    )
-    plt.legend()
-    plt.ylabel('Balanced Accuracy Score')
-    plt.xlabel('Number of Trees')
-    plt.savefig(os.path.join(dt.output_dir(), 'AdaBoostEvol_{:s}.png'.format(comment)))
-    plt.close(fig)    
+
 
 
 #######################################################################
